8d61eb656bd03dd0e14c7d9882c326f5bc65c546
JakkiFx <jackcielfelix555@gmail.com>
2023-09-20 20:28:14 -0300

    salvando os dados no localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-    imgUrl = '/assets/images/bg-desktop-dark.jpg';
-    mode = 'moon';
+  imgUrl = "/assets/images/bg-desktop-dark.jpg";
+  mode = "moon";
+  value: string;
+  index = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   changeMode(): void {
-    if (this.mode === 'sun') {
-      this.mode = 'moon';
+    if (this.mode === "sun") {
+      this.mode = "moon";
     } else {
-      this.mode = 'sun';
+      this.mode = "sun";
     }
   }
 
+  saveToDo(): void {
+    this.index = this.index + 1;
+    let dataToDo = new Array(); // Criando um array para colocar os objetos
+
+    // Verificando se existe a propriedade/array 'dataToDo' no localStorage
+    if (localStorage.hasOwnProperty("dataToDo")) {
+      dataToDo = JSON.parse(localStorage.getItem("dataToDo")); // Pegando os valores da propriedade/array 'dataToDo' e convertendo para JSON
+    }
+
+    dataToDo.push({ id: this.index, do: this.value }); // Adicionando um novo objeto ao array
+
+    localStorage.setItem("dataToDo", JSON.stringify(dataToDo)); // Salvando no localStorage
+  }
+
+  
 }
